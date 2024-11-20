fdded5a91c99fd430b1b895bcd189dad704abe35
AramisHer <aramis.her@outlook.com>
2024-11-20 10:55:32 -0500

    Navbar fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,7 +17,7 @@ export const Header = (props) => {
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Dona ahora!
+                  Contactanos Ahora!
                 </a>{" "}
               </div>
             </div>
