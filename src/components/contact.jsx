bb7d933acce0b453f42df89fbad9ec860c38418b
AramisHer <aramis.her@outlook.com>
2024-11-21 10:19:23 -0500

    Footer updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,9 +153,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2024 Fundación Protégeme. Diseñado por{" "}
+            <a href="https://github.com/Aramisher" rel="nofollow">
+              Aramis Heredia 
             </a>
           </p>
         </div>
