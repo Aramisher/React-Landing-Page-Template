bb5c18f55a51f82b2a2d4339c9f3e3a6f2d38ebb
AramisHer <aramis.her@outlook.com>
2024-11-28 09:40:03 -0500

    Fundadora agregada

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,27 +5,34 @@ export const Team = (props) => {
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Conoce nuestro equipo</h2>
+          <h2>Nuestra Fundadora</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Maestra en Derecho, Promotora y Defensora activa de los Derechos Humanos de grupos
+          vulnerables, reconocida por su distinción a su liderazgo y trayectoria profesional como una de
+          las abogadas más influyentes de México. Doctora Honoris Causa por el Claustro Nacional de
+          Doctores, cuenta con la Certificación en Derechos Humanos ante la Organización de las
+          Naciones Unidas celebrado en La Haya Holanda, Ginebra Suiza, Nuremberg Alemania y
+          Estrasburgo Francia.
           </p>
         </div>
-        <div id="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
-                  </div>
+        <div className="row">
+          {props.data && props.data.length > 0 ? (
+            <div className="col-md-4 col-md-offset-4 team">
+              <div className="thumbnail">
+                <img
+                  src={props.data[0].img}
+                  alt={props.data[0].name}
+                  className="team-img"
+                />
+                <div className="caption">
+                  <h4>{props.data[0].name}</h4>
+                  <p>{props.data[0].job}</p>
                 </div>
-              ))
-            : "loading"}
+              </div>
+            </div>
+          ) : (
+            "loading"
+          )}
         </div>
       </div>
     </div>
